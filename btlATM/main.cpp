e1a5bb4c32bb5f35288a3e29fe7ce5d792dc7756
Tich <vuongtieulam10062005@gmail.com>
2024-10-18 08:51:08 +0700

    update file tiep

--- btlATM/main.cpp
+++ btlATM/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include<iostream>
 #include "ATM.h"
 
 using namespace std;
@@ -6,5 +6,11 @@ using namespace std;
 int main() {
     ATM atm;
     atm.run();
+    
+    // Thêm dòng sau để dừng lại trước khi kết thúc chương trình
+    cout << "Press Enter to exit...";
+    cin.ignore();  // Bỏ qua ký tự enter trước đó (nếu có)
+    cin.get();     // Chờ người dùng nhấn Enter để thoát
     return 0;
+
 }

8053bb0fd8962444c319850a42a5e036e7d81862
Khanh <npbaokhanhpro@gmail.com>
2024-11-09 20:26:04 +0700

    chỉnh 1 chút

--- Updated OOP/Updated OOP/Main.cpp
+++ Updated OOP/Updated OOP/Main.cpp
@@ -7,11 +7,9 @@ using namespace std;
 int main() {
     ATM atm;
     atm.run();
-
-    // Thêm dòng sau ?? d?ng l?i tr??c khi k?t thúc ch??ng trình
     cout << "Press Enter to exit...";
-    cin.ignore();  // B? qua ký t? enter tr??c ?ó (n?u có)
-    cin.get();     // Ch? ng??i dùng nh?n Enter ?? thoát
+    cin.ignore();  
+    cin.get();    
     return 0;
 
 }
--- Updated OOP/Updated OOP/Menu.h
+++ Updated OOP/Updated OOP/Menu.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <string>
 #include <iostream>
+using namespace std;
 
 class Menu {
 public:
-    static void displayHeader(const std::string& title);
+    static void displayHeader(const string& title);
     static void showMenu();
     static void showMenuLoginAfter();
 };
+
+
